2d15c63ee3d75e082349ed4d659c8768714a9a16
Madirex <angel@madirex.com>
2024-02-24 18:20:10 +0100

    fix(models): arreglados problemas modelo categoría

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Clase Category
+ */
 class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    /**
+     * Relación con la tabla Books
+     * @return mixed mixed
+     */
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
+
+    /**
+     * Busca por nombre de categoría
+     * @param $query mixed consulta
+     * @param $search string búsqueda
+     * @return mixed mixed
+     */
+    public function scopeSearch($query, $search)
+    {
+        return $query->whereRaw('LOWER(name) LIKE ?', ["%" . strtolower($search) . "%"]);
+    }
 
 }
