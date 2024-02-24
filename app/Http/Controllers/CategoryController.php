41114d0b904801dc40f3f7c8b1c61e0c88ec81f4
Madirex <angel@madirex.com>
2024-02-24 17:49:27 +0100

    fix(controllers): arreglados fallos categoría

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Book;
 use App\Rules\CategoryNameExists;
 use Illuminate\Http\Request;
 
+/**
+ * Class CategoryController
+ */
 class CategoryController extends Controller
 {
 
+    /**
+     * index
+     * @param Request $request request
+     * @return mixed view or json
+     */
     public function index(Request $request)
     {
-        $categories = Category::all();
+        $categories = Category::search($request->search)->orderBy('name', 'asc')->paginate(8);
+
 
         if ($request->expectsJson()) {
             return response()->json($categories);
@@ -20,17 +30,35 @@ class CategoryController extends Controller
         return view('categories.index', compact('categories'));
     }
 
+    /**
+     * show
+     * @param string $id id
+     * @return mixed view or json
+     */
     public function show(string $id)
     {
         try {
             $category = Category::findOrFail($id);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Categoría no encontrada'], 404);
+            if (request()->expectsJson()) {
+                return response()->json(['message' => 'Categoría no encontrada'], 404);
+            }
+            flash('Categoría no encontrada')->error()->important();
+            return redirect()->back();
         }
-        return response()->json($category);
+
+        if (request()->expectsJson()) {
+            return response()->json($category);
+        }
+
+        return view('categories.show')->with('category', $category);;
     }
 
-
+    /**
+     * store
+     * @param Request $request request
+     * @return mixed view
+     */
     public function store(Request $request)
     {
         try {
@@ -38,22 +66,43 @@ class CategoryController extends Controller
                 'name' => ['required', 'string', new CategoryNameExists, 'max:255']
             ]);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Error al crear la categoría: debe ser única, tener máximo 255 caracteres y no debe estar vacía'], 400);
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'Error al crear la categoría: debe ser única, tener máximo 255 caracteres y no debe estar vacía'], 400);
+            }
+
+            flash('Error al crear la categoría: debe ser única, tener máximo 255 caracteres y no debe estar vacía')->error()->important();
+            return redirect()->back();
         }
         $category = new Category();
         $category->name = $request->input('name');
 
         $category->save();
 
-        return response()->json($category, 201);
+        if ($request->expectsJson()) {
+            return response()->json($category, 201);
+        }
+
+        flash('Categoría creada correctamente')->success();
+        return redirect()->route('categories.index');
     }
 
+    /**
+     * update
+     * @param Request $request request
+     * @param string $id id
+     * @return mixed view
+     */
     public function update(Request $request, string $id)
     {
         try {
             $category = Category::findOrFail($id);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Categoría no encontrada'], 404);
+
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'Categoría no encontrada'], 404);
+            }
+            flash('Categoría no encontrada')->error()->important();
+            return redirect()->back();
         }
 
         try {
@@ -66,24 +115,81 @@ class CategoryController extends Controller
                 'name' => ['required', 'string', $rulesToAdd, 'max:255']
             ]);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Error al crear la categoría: debe ser única, tener máximo 255 caracteres y no debe estar vacía'], 400);
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'Error al crear la categoría: debe ser única, tener máximo 255 caracteres y no debe estar vacía'], 400);
+            }
+            flash('Error al crear la categoría: debe ser única, tener máximo 255 caracteres y no debe estar vacía')->error()->important();
+            return redirect()->back();
         }
 
+        // Obtener todos los books relacionados con la categoría
+        $books = Book::where('category_name', $category->name)->get();
+
         $category->name = $request->input('name');
+
+        // Actualizar el nombre de la categoría en los books relacionados
+        foreach ($books as $book) {
+            $book->category_name = $category->name;
+            $book->save();
+        }
+
         $category->save();
 
-        return response()->json($category);
+        if ($request->expectsJson()) {
+            return response()->json($category);
+        }
+        flash('Categoría actualizada correctamente')->success();
+        return redirect()->route('categories.index');
     }
 
+    /**
+     * destroy
+     * @param string $id id
+     * @return mixed view
+     */
     public function destroy(string $id)
     {
         try {
             $category = Category::findOrFail($id);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'Categoría no encontrada'], 404);
+            if (request()->expectsJson()) {
+                return response()->json(['message' => 'Categoría no encontrada'], 404);
+            }
+            flash('Categoría no encontrada')->error()->important();
+            return redirect()->back();
         }
         $category->delete();
 
-        return response()->json(null, 204);
+        if (request()->expectsJson()) {
+            return response()->json(null, 204);
+        }
+
+        flash('Categoría eliminada correctamente')->success();
+        return redirect()->route('categories.index');
+    }
+
+    /// /// /// /// ///
+    /// PARA VISTAS ///
+    /// /// /// /// ///
+
+    /**
+     * create
+     * @return mixed view
+     */
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    /**
+     * edit
+     * @param $id id
+     * @return mixed view
+     */
+    public function edit($id)
+    {
+        $category = Category::find($id);
+        return view('categories.edit')
+            ->with('category', $category);
     }
 }
