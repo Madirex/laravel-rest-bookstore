16141620007553f22a986e7ea7806f1a8fe65e9a
Madirex <angel@madirex.com>
2024-02-24 18:20:41 +0100

    feat(models): agregado campo User de Roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,9 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+/**
+ * Clase User
+ */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -21,6 +24,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -42,4 +46,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Comprueba si el usuario tiene el rol especificado
+     * @param $role string de rol
+     * @return bool ¿role presente?
+     */
+    public function hasRole($role)
+    {
+        return $this->role === $role;
+    }
 }
