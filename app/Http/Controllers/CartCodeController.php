470b1ad0a886581ea87793464073095973eec008
Madirex <angel@madirex.com>
2024-02-26 13:35:44 +0100

    feat(controller): implementación de vistas en el controlador de CartCode

--- app/Http/Controllers/CartCodeController.php
+++ app/Http/Controllers/CartCodeController.php
@@ -7,26 +7,76 @@ use App\Rules\CartCodeCodeExists;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+/**
+ * Clase CartCodeController
+ */
 class CartCodeController extends Controller
 {
 
-    public function index(): \Illuminate\Http\JsonResponse
+    /**
+     * index
+     * @param Request $request request
+     * @return mixed view or json
+     */
+    public function index(Request $request)
     {
-        $cartcodes = CartCode::all();
+        $cartcodes = CartCode::search($request->search)->orderBy('code', 'asc')->paginate(10);
 
         // Convertir los valores a números flotantes
-        foreach ($cartcodes as $cartcode){
-            $cartcode->percent_discount = (float) $cartcode->percent_discount;
-            $cartcode->fixed_discount = (float) $cartcode->fixed_discount;
+        foreach ($cartcodes as $cartcode) {
+            $cartcode->percent_discount = (float)$cartcode->percent_discount;
+            $cartcode->fixed_discount = (float)$cartcode->fixed_discount;
         }
 
-        return response()->json($cartcodes);
+        if ($request->expectsJson()) {
+            return response()->json($cartcodes);
+        }
+
+        return view('cartcodes.index')->with('cartcodes', $cartcodes);
     }
 
-    public function store(Request $request): \Illuminate\Http\JsonResponse
+    /**
+     * show
+     * @param string $id id
+     * @return mixed view or json
+     */
+    public function show(string $id)
+    {
+        try {
+            $cartcode = CartCode::findOrFail($id);
+        } catch (\Exception $e) {
+            if (request()->expectsJson()) {
+                return response()->json(['message' => 'CartCode no encontrado'], 404);
+            }
+            flash('CartCode no encontrado')->error()->important();
+            return redirect()->back();
+        }
+
+        // Convertir los valores a números flotantes
+        $cartcode->percent_discount = (float)$cartcode->percent_discount;
+        $cartcode->fixed_discount = (float)$cartcode->fixed_discount;
+
+        if (request()->expectsJson()) {
+            return response()->json($cartcode);
+        }
+        return view('cartcodes.show')->with('cartcode', $cartcode);
+    }
+
+    /**
+     * store
+     * @param Request $request request
+     * @return mixed
+     */
+    public function store(Request $request)
     {
         //si no es nulo, retornar
-        if ($errorResponse = $this->validateCartCode($request)) {return $errorResponse;}
+        if ($errorResponse = $this->validateCartCode($request)) {
+            if ($request->expectsJson()) {
+                return $errorResponse;
+            }
+            flash('Error al crear código de tienda: ' . $errorResponse)->error()->important();
+            return redirect()->back();
+        }
         $cartcode = new CartCode();
         $cartcode->code = $request->input('code');
         $cartcode->percent_discount = $request->input('percent_discount');
@@ -37,38 +87,41 @@ class CartCodeController extends Controller
         $cartcode->save();
 
         // Convertir los valores a números flotantes
-        $cartcode->percent_discount = (float) $cartcode->percent_discount;
-        $cartcode->fixed_discount = (float) $cartcode->fixed_discount;
+        $cartcode->percent_discount = (float)$cartcode->percent_discount;
+        $cartcode->fixed_discount = (float)$cartcode->fixed_discount;
 
-        return response()->json($cartcode, 201);
+        if ($request->expectsJson()) {
+            return response()->json($cartcode, 201);
+        }
+        flash('Código de tienda creado correctamente')->success();
+        return redirect()->route('cartcodes.show', $cartcode->id);
     }
 
-
-    public function show(string $id): \Illuminate\Http\JsonResponse
+    /**
+     * update
+     * @param Request $request request
+     * @param string $id id
+     * @return mixed view or json
+     */
+    public function update(Request $request, string $id)
     {
         try {
             $cartcode = CartCode::findOrFail($id);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'CartCode no encontrado'], 404);
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'CartCode no encontrado'], 404);
+            }
+            flash('Código de tienda no encontrado')->error()->important();
+            return redirect()->back();
         }
 
-        // Convertir los valores a números flotantes
-        $cartcode->percent_discount = (float) $cartcode->percent_discount;
-        $cartcode->fixed_discount = (float) $cartcode->fixed_discount;
-
-        return response()->json($cartcode);
-    }
-
-
-    public function update(Request $request, string $id): \Illuminate\Http\JsonResponse
-    {
-        try {
-            $cartcode = CartCode::findOrFail($id);
-        } catch (\Exception $e) {
-            return response()->json(['message' => 'CartCode no encontrado'], 404);
+        if ($errorResponse = $this->validateCartCode($request, $cartcode->code)) {
+            if ($request->expectsJson()) {
+                return $errorResponse;
+            }
+            flash('Error al actualizar el código de tienda: ' . $errorResponse)->error()->important();
+            return redirect()->back();
         }
-
-        if ($errorResponse = $this->validateCartCode($request, $cartcode->code)) {return $errorResponse;}
 
         $cartcode->code = $request->input('code');
         $cartcode->percent_discount = $request->input('percent_discount');
@@ -78,66 +131,130 @@ class CartCodeController extends Controller
         $cartcode->save();
 
         // Convertir los valores a números flotantes
-        $cartcode->percent_discount = (float) $cartcode->percent_discount;
-        $cartcode->fixed_discount = (float) $cartcode->fixed_discount;
+        $cartcode->percent_discount = (float)$cartcode->percent_discount;
+        $cartcode->fixed_discount = (float)$cartcode->fixed_discount;
 
-        return response()->json($cartcode);
+        if ($request->expectsJson()) {
+            return response()->json($cartcode);
+        }
+        flash('Código de tienda actualizado correctamente')->success();
+        return redirect()->route('cartcodes.show', $cartcode->id);
     }
 
-    public function destroy(string $id): \Illuminate\Http\JsonResponse
+    /**
+     * destroy
+     * @param string $id id
+     * @return mixed view or json
+     */
+    public function destroy(string $id)
     {
         try {
             $cartcode = CartCode::findOrFail($id);
         } catch (\Exception $e) {
-            return response()->json(['message' => 'CartCode no encontrado'], 404);
+
+            if (request()->expectsJson()) {
+                return response()->json(['message' => 'CartCode no encontrado'], 404);
+            }
+            flash('Código de tienda no encontrado')->error()->important();
+            return redirect()->back();
         }
         $cartcode->delete();
 
-        return response()->json(null, 204);
+        if (request()->expectsJson()) {
+            return response()->json(null, 204);
+        }
+        flash('Código de tienda eliminado correctamente')->success();
+        return redirect()->route('cartcodes.index');
     }
 
-    public function validateCartCode(Request $request, $codestr = null): \Illuminate\Http\JsonResponse | null
+    /**
+     * validateCartCode
+     * @param Request $request request
+     * @param string|null $codestr codestr
+     * @return mixed null or json
+     */
+    public function validateCartCode(Request $request, $codestr = null)
     {
-            $rulesToAdd = '';
-            if ($codestr != null){
-                if ($request->code != $codestr){
-                    $rulesToAdd = new CartCodeCodeExists;
-                }
-            }else{
+        $rulesToAdd = '';
+        if ($codestr != null) {
+            if ($request->code != $codestr) {
                 $rulesToAdd = new CartCodeCodeExists;
             }
+        } else {
+            $rulesToAdd = new CartCodeCodeExists;
+        }
 
-            try{
-                $validator = Validator::make($request->all(), [
-                    'code' => ['required', 'string', $rulesToAdd, 'max:255'],
-                    'percent_discount' => 'required|numeric|min:0|max:999.99|regex:/^\d{1,3}(\.\d{1,2})?$/',
-                    'fixed_discount' => 'required|numeric|min:0|max:999999.99|regex:/^\d{1,6}(\.\d{1,2})?$/',
-                    'available_uses' => 'required|integer|min:0|max:1000000000',
-                    'expiration_date' => 'required|date',
-                ]);
+        try {
+            $validator = Validator::make($request->all(), [
+                'code' => ['required', 'string', $rulesToAdd, 'max:255'],
+                'percent_discount' => 'required|numeric|min:0|max:999.99|regex:/^\d{1,3}(\.\d{1,2})?$/',
+                'fixed_discount' => 'required|numeric|min:0|max:999999.99|regex:/^\d{1,6}(\.\d{1,2})?$/',
+                'available_uses' => 'required|integer|min:0|max:1000000000',
+                'expiration_date' => 'required|date',
+            ]);
 
-                if ($validator->fails()) {
-                    $errors = $validator->errors()->all();
+            if ($validator->fails()) {
+                $errors = $validator->errors()->all();
+
+                if ($request->expectsJson()) {
                     return response()->json(['errors' => $errors], 400);
                 }
-            } catch (\Brick\Math\Exception\NumberFormatException $e) {
+
+                return implode(' ', $errors);
+            }
+        } catch (\Brick\Math\Exception\NumberFormatException $e) {
+
+            if ($request->expectsJson()) {
                 return response()->json(['message' => 'Error al procesar una propiedad por no tener un número válido. Evita que exceda del tamaño límite.'], 400);
             }
 
-            // Convertir los valores a números flotantes
-            $request->percent_discount = (float) $request->percent_discount;
-            $request->fixed_discount = (float) $request->fixed_discount;
+            return 'Error al procesar una propiedad por no tener un número válido. Evita que exceda del tamaño límite.';
+        }
 
-            // Forzar a que el descuento no pueda tener más de 100% de descuento
-            if ($request->percent_discount > 100){
+        // Convertir los valores a números flotantes
+        $request->percent_discount = (float)$request->percent_discount;
+        $request->fixed_discount = (float)$request->fixed_discount;
+
+        // Forzar a que el descuento no pueda tener más de 100% de descuento
+        if ($request->percent_discount > 100) {
+            if ($request->expectsJson()) {
                 return response()->json(['message' => 'El descuento por porcentaje no puede ser mayor a 100%'], 400);
             }
+            return 'El descuento por porcentaje no puede ser mayor a 100%';
+        }
 
-
-            if ($validator->fails()) {
+        if ($validator->fails()) {
+            if ($request->expectsJson()) {
                 return response()->json(['message' => $validator->errors()->first()], 400);
-            }else{
-                return null;
             }
+            return $validator->errors()->first();
+        } else {
+            return null;
+        }
+    }
+
+    /// /// /// /// ///
+    /// PARA VISTAS ///
+    /// /// /// /// ///
+
+    /**
+     * create
+     * @return mixed view
+     */
+    public function create()
+    {
+        return view('cartcodes.create');
+    }
+
+    /**
+     * edit
+     * @param $id id
+     * @return mixed view
+     */
+    public function edit($id)
+    {
+        $cartcode = CartCode::find($id);
+        return view('cartcodes.edit')
+            ->with('cartcode', $cartcode);
     }
 }
