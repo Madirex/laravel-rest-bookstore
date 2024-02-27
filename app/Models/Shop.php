82c4ac0d27eda5d18a4eae59ae9030418a67b2c0
Alexandru <alexandrudorobat@gmail.com>
2024-02-27 19:47:51 +0100

    feat(model): creado modelo de shop

---
+++ app/Models/Shop.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shop extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [ 'name', 'address', 'books', 'active'];
+
+    /**
+     * Oculta los campos
+     * @var string[] $hidden
+     */
+    protected $hidden = [
+        'active',
+    ];
+
+    /**
+     * Convierte en tipos nativos
+     * @var string[] $casts
+     */
+    protected $casts = [
+        'active' => 'boolean',
+    ];
+
+    /**
+     * Relación con la tabla libros
+     * @return mixed mixed
+     */
+    public function book()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Busca por nombre de Shop
+     * @param $query mixed consulta
+     * @param $search string búsqueda
+     * @return mixed mixed
+     */
+
+    public function scopeSearch($query, $search)
+    {
+        return $query->whereRaw('LOWER(name) LIKE ?', ["%" . strtolower($search) . "%"])
+            ->orWhereRaw('LOWER(address) LIKE ?', ["%" . strtolower($search) . "%"]);
+    }
+
+}
