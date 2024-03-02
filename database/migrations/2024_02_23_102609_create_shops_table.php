a44155743a9bb6873aa2a192b5d65d397107bdda
Madirex <angel@madirex.com>
2024-03-03 00:44:36 +0100

    fix(migrations): arreglos en migrations

--- database/migrations/2024_02_23_102609_create_shops_table.php
+++ database/migrations/2024_02_23_102609_create_shops_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->json('address');
             $table->boolean('active')->default(true);
             $table->timestamps();
         });
