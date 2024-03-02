96083da26d40ac13edef4b6ea687f87cdc2b3cc6
Madirex <madirexland@gmail.com>
2024-03-02 18:20:19 +0100

    Revert "Develop"

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,21 +2,15 @@
 
 namespace App\Models;
 
-use App\Notifications\VerifyEmailCustom;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-/**
- * Clase User
- */
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
-    public static $IMAGE_DEFAULT = 'images/user.png';
 
     /**
      * The attributes that are mass assignable.
@@ -27,24 +21,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'password',
-        'role',
-        'surname',
-        'username',
-        'isDeleted',
-        'phone',
-        'image',
-        'cart',
-        'orders',
     ];
-
-    /**
-     * Relación con la tabla Address
-     * @return mixed mixed
-     */
-    public function address()
-    {
-        return $this->morphOne(Address::class, 'addressable');
-    }
 
     /**
      * The attributes that should be hidden for serialization.
@@ -65,36 +42,4 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
-    /**
-     * Comprueba si el usuario tiene el rol especificado
-     * @param $role string de rol
-     * @return bool ¿role presente?
-     */
-    public function hasRole($role)
-    {
-        return $this->role === $role;
-    }
-
-    /**
-     * Busca por username/email de User
-     * @param $query mixed consulta
-     * @param $search string búsqueda
-     * @return mixed mixed
-     */
-    public function scopeSearch($query, $search)
-    {
-        return $query->whereRaw('LOWER(username) LIKE ?', ["%" . strtolower($search) . "%"])
-            ->orWhereRaw('LOWER(email) LIKE ?', ["%" . strtolower($search) . "%"]);
-    }
-
-    /**
-     * Send the email verification notification.
-     *
-     * @return void
-     */
-    public function sendEmailVerificationNotification()
-    {
-        $this->notify(new VerifyEmailCustom);
-    }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,13 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call([
-            CategoriesTableSeeder::class,
-            CartCodesTableSeeder::class,
-            BooksTableSeeder::class,
-            UserSeeder::class,
-            CartSeeder::class,
-            AddressTableSeeder::class,
-        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
