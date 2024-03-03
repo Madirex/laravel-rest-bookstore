3e9c152c9c64c88b558656bcefeb22d281bc6828
Madirex <angel@madirex.com>
2024-03-03 19:09:55 +0100

    views(books): mejorado estilo y conexión con otras vistas en Book Show

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,38 +1,51 @@
 @php use App\Models\Book; @endphp
+
 @extends('main')
+
 @section('title', 'Detalles Book')
+
 @section('content')
-    <h1>Detalles del Book</h1>
+    <br/>
+    <h1><i class="fas fa-tag"></i> {{ $book->name }}</h1>
+    <br/>
     <div class="card">
-        <div class="card-body">
-            <h5 class="card-title"><i class="fas fa-tag"></i> {{ $book->name }}</h5>
-            <p class="card-text"><i class="fas fa-info-circle"></i> Descripción: {{ $book->description }}</p>
-            <p class="card-text"><i class="fas fa-barcode"></i> ISBN: {{ $book->isbn }}</p>
-            <p class="card-text"><i class="fas fa-user"></i> Autor: {{ $book->author }}</p>
-            <p class="card-text"><i class="fas fa-building"></i> Editorial: {{ $book->publisher }}</p>
-            <p class="card-text"><i class="fas fa-store"></i> Tienda: {{ $book->shop->name }}</p>
-            <p class="card-text"><i class="fas fa-euro-sign"></i> Precio: {{ $book->price }}</p>
-            <p class="card-text">
+        <div class="row">
+            <div class="col-md-4">
                 @if($book->image != Book::$IMAGE_DEFAULT)
-                    <img alt="Imagen del Book" class="img-fluid book-image" src="{{ asset('storage/' . $book->image) }}">
+                    <img alt="Imagen del Book" style="padding-left: 40px;padding-top: 40px;" class="img-fluid book-image" src="{{ asset('storage/' . $book->image) }}">
                 @else
-                    <img alt="Imagen por defecto" class="img-fluid book-image" src="{{ Book::$IMAGE_DEFAULT }}">
+                    <img alt="Imagen por defecto" style="padding-left: 40px;padding-top: 40px;" class="img-fluid book-image" src="{{ Book::$IMAGE_DEFAULT }}">
                 @endif
-            </p>
-            <p class="card-text"><i class="fas fa-layer-group"></i> Stock: {{ $book->stock }}</p>
-            <p class="card-text"><i class="fas fa-folder-open"></i> Categoría: {{ $book->category_name }}</p>
+                <div class="row-cols-bg" style="padding-left: 40px;padding-top: 40px; padding-bottom: 40px;">
+                    <p class="card-text"><i class="fas fa-user"></i> Autor: {{ $book->author }}</p>
+                    <p class="card-text"><i class="fas fa-building"></i> Editorial: {{ $book->publisher }}</p>
+                    <p class="card-text"><i class="fas fa-barcode"></i> ISBN: {{ $book->isbn }}</p>
+                </div>
+            </div>
+            <div class="col-md-8" >
+                <div class="card-body" style="padding-top: 60px">
+                    <p><i class="fas fa-store"></i> Tienda: <a style="text-decoration:none;" href="{{ route('shops.show', $book->shop->id) }}" class="card-text">{{ $book->shop->name }}</a></p>
+                    <p><i class="fas fa-tag"></i> Categoría: <a style="text-decoration:none;" href="{{ route('categories.show', $book->category->id) }}" class="card-text">{{ $book->category_name }}</a></p>
+                    <p class="card-text"><i class="fas fa-info-circle"></i> Descripción: {{ $book->description }}</p>
+                    <!-- categoría con enlace -->
+                    <p><i class="fas fa-euro-sign"></i> {{ $book->price }}</p>
+                    <p><i class="fas fa-layer-group"></i> Stock: {{ $book->stock }}</p>
+                </div>
+                @if(auth()->check() && auth()->user()->hasVerifiedEmail() && $book->stock > 0)
+                    <form action="{{ route('cart.handle') }}" method="POST" style="display:inline;">
+                        @csrf
+                        <input type="hidden" name="book_id" value="{{ $book->id }}">
+                        <input type="hidden" name="action" value="add">
+                        <button type="submit" class="btn btn-success btn-lg">Agregar al carrito <i class="fas fa-cart-plus"></i></button>
+                    </form>
+                @endif
+            </div>
+
         </div>
     </div>
 
     <br/>
-    @if(auth()->check() && auth()->user()->hasVerifiedEmail() && $book->stock > 0)
-        <form action="{{ route('cart.handle') }}" method="POST" style="display:inline;">
-            @csrf
-            <input type="hidden" name="book_id" value="{{ $book->id }}">
-            <input type="hidden" name="action" value="add">
-            <button type="submit" class="btn btn-success btn-sm">Agregar al carrito <i class="fas fa-cart-plus"></i></button>
-        </form>
-    @endif
+
 
     <br/><br/>
     @if(auth()->check() && auth()->user()->hasRole('admin'))
