1db3df203e249d90d094971eef82f879d003f89a
Madirex <angel@madirex.com>
2024-03-05 10:35:49 +0100

    views(orders): agregado botón de editar en vista de pedido

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -11,11 +11,11 @@
             @if ($order->address)
                 <h2>Dirección</h2>
                 <div class="mb-5">
-                    <p>Calle:  {{ $order->address->street }}</p>
+                    <p>Calle: {{ $order->address->street }}</p>
                     <p>Ciudad: {{ $order->address->city }}</p>
                     <p>Provincia: {{ $order->address->province }}</p>
                     <p>Country: {{ $order->address->country }}</p>
-                    <p>CP:     {{ $order->address->postal_code }}</p>
+                    <p>CP: {{ $order->address->postal_code }}</p>
                 </div>
             @endif
             <h2>Usuario: {{ $order->user->name }}</h2>
@@ -26,27 +26,33 @@
             <h3>Pedidos</h3>
             <table class="table table-striped">
                 <thead>
-                    <tr>
-                        <th scope="col">ID</th>
-                        <th scope="col">Nombre</th>
-                        <th scope="col">Precio</th>
-                        <th scope="col">Cantidad</th>
-                        <th scope="col">Subtotal</th>
-                    </tr>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Cantidad</th>
+                    <th scope="col">Subtotal</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {{-- Por cada order --}}
-                    @foreach ($order->orderLines as $orderLine)
-                        <tr>
-                            <th scope="row">{{ $orderLine->id }}</th>
-                            <td>{{ $orderLine->book->name }}</td>
-                            <td>{{ $orderLine->price }} €</td>
-                            <td>{{ $orderLine->quantity }}</td>
-                            <td>{{ $orderLine->subtotal }} €</td>
-                        </tr>
-                    @endforeach
+                {{-- Por cada order --}}
+                @foreach ($order->orderLines as $orderLine)
+                    <tr>
+                        <th scope="row">{{ $orderLine->id }}</th>
+                        <td>{{ $orderLine->book->name }}</td>
+                        <td>{{ $orderLine->price }} €</td>
+                        <td>{{ $orderLine->quantity }}</td>
+                        <td>{{ $orderLine->subtotal }} €</td>
+                    </tr>
+                @endforeach
                 </tbody>
             </table>
+        </div>
+
+        <!-- editar este pedido -->
+        <div class="card-footer">
+            <a class="btn btn-secondary" href="{{ route('orders.edit', $order->id) }}"><i class="fas fa-edit"></i>
+                Editar</a>
         </div>
     </div>
 @endsection
